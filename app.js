45f13542624e7f6df7412d8ce232273442bb0d22
Rasprit Kaur <kaur.rasprit@gmail.com>
2017-08-01 16:41:31 -0400

    failed attempt at sockets

--- app.js
+++ app.js
@@ -2,27 +2,26 @@ const http = require('http');
 const express = require( 'express' );
 const nunjucks = require('nunjucks');
 const app = express(); // creates an instance of an express application
-const server = http.createServer();
 const bodyParser = require('body-parser');
-
+var socketio = require('socket.io');
 const routes = require('./routes');
-app.use(express.static(__dirname + '/public'));
+app.engine('html', nunjucks.render);
+app.set('view engine', 'html');
+nunjucks.configure('views', {noCache: true});
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.use('/', routes);
-
-app.engine('html', nunjucks.render);
-app.set('view engine', 'html');
-
-nunjucks.configure('views', {noCache: true});
-
-server.listen(3000, function(){
+var server = app.listen(3000, function(){
     console.log('Listening on port 3000..');
 });
+
+app.use(express.static(__dirname + '/public'));
+var io = socketio.listen(server);
+app.use('/', routes(io));
+
 
 app.use(function(req, res, next){
     //do your logging here
@@ -30,7 +29,5 @@ app.use(function(req, res, next){
     console.log(req.method + ' ' + req.url);
     next();
 })
-
-
 
 server.on('request', app);
