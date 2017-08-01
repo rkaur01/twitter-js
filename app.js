1c7f096c84f56bed96fb353707ce5004fac1ba3e
Rasprit Kaur <kaur.rasprit@gmail.com>
2017-08-01 10:39:47 -0400

    completed template rendering

--- app.js
+++ app.js
@@ -12,26 +12,20 @@ var locals = {
     ]
 }
 
+app.engine('html', nunjucks.render);
+app.set('view engine', 'html');
 
+nunjucks.configure('views', {noCache: true});
 
 server.listen(3000, function(){
     console.log('Listening on port 3000..');
 });
 
-app.set('view engine', 'html');
-app.engine('html', nunjucks.render);
-
-nunjucks.configure('views', {noCache: true});
-nunjucks.render('index.html', locals, function(err, output) {
-    if (err) { console.log(err.message)}
-    else console.log(output);
-});
-
-
 app.use(function(req, res, next){
     //do your logging here
     //call 'next' or your app will be a black hole
     console.log(req.method + ' ' + req.url);
+    next();
 })
 
 app.get('/', function(req, res){
@@ -41,14 +35,5 @@ app.get('/', function(req, res){
 app.get('/news', function(req, res){
      res.render('index', {title: 'Daily News', people: locals.people});
 });
-
-// app.get('/is-anybody-in-there', function(req, res){
-//     res.send('Hello? Anyone here?\n');
-// });
-
-// app.post('/modernism', function(req, res, next) {
-//   cartoons.push(req.body.cartoon);
-//   res.sendStatus(201);
-// })
 
 server.on('request', app);
